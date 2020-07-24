a97da621c988b21ba6617d680c665833eb16ff82
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-07-25 00:47:12 +0530

    d

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,6 +1,6 @@
 var matches = document.querySelectorAll("#preload")
 window.addEventListener('load',function(){
-    setTimeout(function(){ matches[0].style.display = 'none'; }, 3000);
+    setTimeout(function(){ matches[0].style.display = 'none'; }, 0000);
 });
 
 
