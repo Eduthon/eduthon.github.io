3a91e12af7ee1a4a4ef7f799042d6301c265640c
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-07-30 17:05:12 +0530

    sdfdsafs

---
+++ assets/js/pcardscript.js
@@ -0,0 +1,89 @@
+/*global $, console*/
+
+$(function () {
+   
+    // 'use strict';
+    
+    $('#tid1').click(function () {
+        $('#tid1').toggleClass('pCard_on');
+        
+    });
+    $('#tid2').click(function () {
+        $('#tid2').toggleClass('pCard_on');
+        
+    });
+    $('#tid3').click(function () {
+        $('#tid3').toggleClass('pCard_on');
+        
+    });
+    $('#tid4').click(function () {
+        $('#tid4').toggleClass('pCard_on');
+        
+    });
+    $('#tid5').click(function () {
+        $('#tid5').toggleClass('pCard_on');
+        
+    });
+    $('#tid6').click(function () {
+        $('#tid6').toggleClass('pCard_on');
+        
+    });
+    $('#tid7').click(function () {
+        $('#tid7').toggleClass('pCard_on');
+        
+    });
+    $('#tid8').click(function () {
+        $('#tid8').toggleClass('pCard_on');
+        
+    });
+    $('#tid9').click(function () {
+        $('#tid9').toggleClass('pCard_on');
+        
+    });
+    $('#tid10').click(function () {
+        $('#tid10').toggleClass('pCard_on');
+        
+    });
+    $('#tid12').click(function () {
+        $('#tid12').toggleClass('pCard_on');
+        
+    });
+    $('#tid13').click(function () {
+        $('#tid13').toggleClass('pCard_on');
+        
+    });
+    $('#tid11').click(function () {
+        $('#tid11').toggleClass('pCard_on');
+        
+    });
+    $('#tid14').click(function () {
+        $('#tid14').toggleClass('pCard_on');
+        
+    });
+    $('#tid15').click(function () {
+        $('#tid15').toggleClass('pCard_on');
+        
+    });
+    $('#tid16').click(function () {
+        $('#tid16').toggleClass('pCard_on');
+        
+    });
+    $('#tid17').click(function () {
+        $('#tid17').toggleClass('pCard_on');
+        
+    });
+
+
+
+
+});
+
+
+
+// function myFunction() {
+//     var elements = document.querySelectorAll(".pCard_add");
+//     elements.forEach(element => {
+//         element.addEventListener("click", function(){ element.classList.toggle("pCard_on"); }); 
+//     })
+
+//   } 
