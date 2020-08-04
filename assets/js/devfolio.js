f147a27dc5075a358a64b4be4605a11994602337
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-08-04 15:03:10 +0530

    dsfsdf

--- assets/js/devfolio.js
+++ assets/js/devfolio.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     let devfolioOptions = {
         buttonSelector: '#devfolio-apply-now',
-        key: 'myhackathonkey',
+        key: 'eduthon',
     }
 
     let script = document.createElement('script');
