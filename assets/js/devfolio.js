4859202a3f17e4de17dac12ffdf3209133e5c0dc
blurry-x-face <rishabhshukla1999@gmail.com>
2020-08-24 14:20:56 +0530

    fix devfolio overflow

--- assets/js/devfolio.js
+++ assets/js/devfolio.js
@@ -1,40 +1,23 @@
-if(window.innerWidth>= 600)
-{ document.addEventListener('DOMContentLoaded', function () {
-    let devfolioOptions = {
-        buttonSelector: '#devfolio-apply-now',
-        key: 'eduthon',
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  let devfolioOptions = {
+    buttonSelector: "#devfolio-apply-now",
+    key: "eduthon",
+  };
 
-    let script = document.createElement('script');
-    script.src = "https://apply.devfolio.co";
-    document.head.append(script);
+  let script = document.createElement("script");
+  script.src = "https://apply.devfolio.co";
+  document.head.append(script);
 
-    script.onload = function () {
-        new Devfolio(devfolioOptions);
-    }
+  script.onload = function () {
+    new Devfolio(devfolioOptions);
+  };
 
-    script.onerror = function () {
-        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
-            window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
-        });
-    }
-}); 
-}
-else{
-    let devfolioOptions = {
-        buttonSelector: '#devfolio-apply-now',
-        key: 'eduthon',
-    }
-    
-    let script = document.createElement('script');
-    script.src = "https://apply.devfolio.co";
-    document.head.append(script);
-
-    script.onload = function () {
-        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
-            window.open( 
-                'https://devfolio.co/external-apply/' +  devfolioOptions.key, "_blank");
-        });
-    }
-    
-}
+  script.onerror = function (e) {
+    document
+      .querySelector(devfolioOptions.buttonSelector)
+      .addEventListener("click", function () {
+        window.location.href =
+          "https://devfolio.co/external-apply/" + devfolioOptions.key;
+      });
+  };
+});
